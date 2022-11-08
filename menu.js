29b8c9eb038556e4b55fdddb74bd2ca5e6873bd2
p2109548 <noah.heraud@etu.univ-lyon1.fr>
2022-11-08 17:40:28 +0100

    creation du canvas via js

--- menu.js
+++ menu.js
@@ -1,5 +1,14 @@
+var play = document.querySelector("#play");
 var page = document.querySelector("#page");
-page.innerHTML +=
-	'<canvas id="zone" width="400" height="400" style="background-color:#2c3e50;margin:0 auto; "></canvas>';
-var zone = document.querySelector("#zone");
-var context = zone.getContext("2d");
+var play = false;
+
+play.addEventListener("click", () => {
+	page.innerHTML +=
+		'<canvas id="zone" width="400" height="400" style="background-color:#2c3e50;margin:0 auto; "></canvas>';
+	play = true;
+});
+
+if (play) {
+	var zone = document.querySelector("#zone");
+	var context = zone.getContext("2d");
+}
