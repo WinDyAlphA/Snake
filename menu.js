ef2aee6d91a1616e5c14d9b1088d640a33c69736
p2109548 <noah.heraud@etu.univ-lyon1.fr>
2022-11-08 19:39:43 +0100

    canva fonctionnel

--- menu.js
+++ menu.js
@@ -1,14 +1,13 @@
-var play = document.querySelector("#play");
 var page = document.querySelector("#page");
-var play = false;
 
-play.addEventListener("click", () => {
-	page.innerHTML +=
+document.querySelector("#play").addEventListener("click", () => {
+	page.innerHTML =
 		'<canvas id="zone" width="400" height="400" style="background-color:#2c3e50;margin:0 auto; "></canvas>';
-	play = true;
+	play();
 });
 
-if (play) {
+const play = () => {
 	var zone = document.querySelector("#zone");
 	var context = zone.getContext("2d");
-}
+	console.log("aaa");
+};
