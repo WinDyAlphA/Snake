b0145fffa7415abbec285026f9f5437f55dba527
p2109548 <noah.heraud@etu.univ-lyon1.fr>
2022-11-09 18:15:18 +0100

    rajout des murs

--- menu.js
+++ menu.js
@@ -4,14 +4,22 @@ window.onload = function () {
 	var nbPomme = document.querySelector("#nbPomme");
 	var vitesse = document.querySelector("#vitesse");
 	var color = document.querySelector("#color");
+	var mur = document.querySelector("#mur");
+	var boolMur = false;
+
 	let compteur = 0;
 	document.querySelector("#play").addEventListener("click", (e) => {
-		play(nbPomme.value, vitesse.value, color.value);
+		if (mur.checked) {
+			boolMur = true;
+		} else {
+			boolMur = false;
+		}
+		play(nbPomme.value, vitesse.value, color.value, boolMur);
 	});
 };
 
-const play = (nbFruits, timingstamp, color) => {
-	console.log(nbFruits, timingstamp);
+const play = (nbFruits, timingstamp, color, boolMur) => {
+	console.log(nbFruits, timingstamp, boolMur);
 	timingstamp = 10 - timingstamp;
 	page.innerHTML =
 		'<div id="score">Score: <span id="scoreNum">0</span></div><div id="high">High Score: <span id="highNum">0</span></div><canvas id="zone" width="400" height="400" style="background-color:#2c3e50;margin:0 auto; "></canvas><button id="pause">pause</button>';
@@ -40,6 +48,7 @@ const play = (nbFruits, timingstamp, color) => {
 	var tabFood = [];
 
 	function createFood() {
+		//création de la nourriture selon nbFruits
 		for (let i = 0; i < nbFruits; i++) {
 			tabFood[i] = {
 				x: getRandomInt(0, 25) * grid,
@@ -53,6 +62,7 @@ const play = (nbFruits, timingstamp, color) => {
 		return Math.floor(Math.random() * (max - min)) + min;
 	}
 	function snakeOver(snake) {
+		//remise à zéro du serpent
 		snake.x = 160;
 		snake.y = 160;
 		snake.cells = [];
@@ -83,40 +93,8 @@ const play = (nbFruits, timingstamp, color) => {
 		} else if (snake.x >= canvas.width) {
 			snake.x = 0;
 		}
-		var border = false;
-		// si le serpent sort de l'écran, il réapparait de l'autre côté vertcialement
-		if (border == false) {
-			if (snake.y < 0) {
-				snake.y = canvas.height - grid;
-			} else if (snake.y >= canvas.height) {
-				snake.y = 0;
-			}
-		} else if (border == true) {
-			console.log(
-				snake.y +
-					" --- " +
-					snake.x +
-					" .           ." +
-					canvas.height +
-					" --- " +
-					canvas.width
-			);
-			if (
-				snake.y < 0 ||
-				snake.x < 0 ||
-				snake.x >= 384 ||
-				snake.y >= canvas.height
-			) {
-				snakeOver(snake);
-				score = 0;
-				for (var i = 0; i < tabFood.length; i++) {
-					tabFood[i].x = getRandomInt(0, 25) * grid;
-					tabFood[i].y = getRandomInt(0, 25) * grid;
-				}
-
-				document.getElementById("highNum").innerHTML = "&nbsp;" + max;
-			}
-		}
+		var border = boolMur;
+		//si le serpent sort de l'écran, il réapparait de l'autre côté vertcialement
 
 		// garde en memoire les deplacement du serpent. debut du tableau = tete du serpent
 		snake.cells.unshift({ x: snake.x, y: snake.y });
@@ -149,19 +127,53 @@ const play = (nbFruits, timingstamp, color) => {
 			// verifier collision avec le serpent
 			for (var i = index + 1; i < snake.cells.length; i++) {
 				// si la tete du serpent est sur une autre cellule du serpent, le jeu est perdu
-				if (cell.x === snake.cells[i].x && cell.y === snake.cells[i].y) {
-					if (score > max) {
-						max = score;
-					}
-					snakeOver(snake);
-					score = 0;
-					for (var i = 0; i < tabFood.length; i++) {
-						tabFood[i].x = getRandomInt(0, 25) * grid;
-						tabFood[i].y = getRandomInt(0, 25) * grid;
-					}
+				if (border == true) {
+					if (
+						//condition pour que le bords de l'ecran soit solide +lui meme
+						(cell.x === snake.cells[i].x && cell.y === snake.cells[i].y) ||
+						snake.y < 0 ||
+						snake.x < 0 ||
+						(snake.cells[0].x == 0 && snake.cells[0 + 1].x == 384) ||
+						(snake.cells[0].x == 384 && snake.cells[0 + 1].x == 0) ||
+						snake.y >= canvas.height
+					) {
+						if (score > max) {
+							max = score;
+						}
+						snakeOver(snake);
+						score = 0;
+						for (var i = 0; i < tabFood.length; i++) {
+							tabFood[i].x = getRandomInt(0, 25) * grid;
+							tabFood[i].y = getRandomInt(0, 25) * grid;
+						}
 
-					document.getElementById("highNum").innerHTML = "&nbsp;" + max;
-					document.getElementById("scoreNum").innerHTML = "&nbsp;" + 0;
+						document.getElementById("highNum").innerHTML = "&nbsp;" + max;
+						document.getElementById("scoreNum").innerHTML = "&nbsp;" + 0;
+					}
+				}
+
+				if (border == false) {
+					//gestion des bordures, il reaparait de l'autre cote
+					if (snake.y < 0) {
+						snake.y = canvas.height - grid;
+					} else if (snake.y >= canvas.height) {
+						snake.y = 0;
+					}
+					//si la tete du serpent est sur une autre cellule du serpent, le jeu est perdu
+					if (cell.x === snake.cells[i].x && cell.y === snake.cells[i].y) {
+						if (score > max) {
+							max = score;
+						}
+						snakeOver(snake);
+						score = 0;
+						for (var i = 0; i < tabFood.length; i++) {
+							tabFood[i].x = getRandomInt(0, 25) * grid;
+							tabFood[i].y = getRandomInt(0, 25) * grid;
+						}
+						//noter le score
+						document.getElementById("highNum").innerHTML = "&nbsp;" + max;
+						document.getElementById("scoreNum").innerHTML = "&nbsp;" + 0;
+					}
 				}
 			}
 		});
