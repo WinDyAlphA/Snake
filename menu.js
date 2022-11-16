08d03b06c2da1cbdd50e1f8c249a8c5f58510c98
p2109548 <noah.heraud@etu.univ-lyon1.fr>
2022-11-16 23:32:20 +0100

    sprite ajouté

--- menu.js
+++ menu.js
@@ -107,10 +107,11 @@ const play = (nbFruits, timingstamp, color, boolMur, ia, pixels, randInt) => {
 	timingstamp = 10 - timingstamp;
 	console.log(pixels);
 	page.innerHTML =
-		'<div id="affichage"><div id="score" class="score">Score :&nbsp<span id="scoreNum">0</span></div><div id="high" class="high">High Score :&nbsp<span id="highNum">0</span></div></div><div id="canvas"><canvas id="zone" width="400" height="400"></canvas></div><div id="BTN-jouer"><button id="pause">Pause</button><button id="return">Retour</button></div>';
+		'<div id="affichage"><div id="score" class="score">Score :&nbsp<span id="scoreNum">0</span></div><div id="high" class="high">High Score :&nbsp<span id="highNum">0</span></div></div><div id="canvas"><canvas id="zone" width="400" height="400"></canvas></div><img id="source" style="display:none;" src="https://rembound.com/files/creating-a-snake-game-tutorial-with-html5/snake-graphics.png"width="320" height="256"><div id="BTN-jouer"><button id="pause">Pause</button><button id="return">Retour</button></div>';
 
 	var canvas = document.getElementById("zone");
 	var context = canvas.getContext("2d");
+	var image = document.getElementById("source");
 	var grid = pixels;
 	var count = 0;
 	var score = 0;
@@ -239,9 +240,19 @@ const play = (nbFruits, timingstamp, color, boolMur, ia, pixels, randInt) => {
 			snake.cells.pop();
 		}
 		// Dessine la nourriture
-		context.fillStyle = "white";
+
 		for (var i = 0; i < tabFood.length; i++) {
-			context.fillRect(tabFood[i].x, tabFood[i].y, grid - 1, grid - 1);
+			context.drawImage(
+				image,
+				0 * 64,
+				3 * 64,
+				64,
+				64,
+				tabFood[i].x,
+				tabFood[i].y,
+				16,
+				16
+			);
 		}
 		//dessine les mur tabMur
 		context.fillStyle = "black";
@@ -288,11 +299,12 @@ const play = (nbFruits, timingstamp, color, boolMur, ia, pixels, randInt) => {
 			return result;
 		}
 
+		var indexCopy;
 		snake.cells.forEach(function (cell, index) {
+			indexCopy = index;
 			//creation d'une inteligence artificielle qui joue asnake et veut recuperer le plus de nourriture possible
 
 			// dessine le serpent avec un padding de 1px
-			context.fillRect(cell.x, cell.y, grid - 1, grid - 1);
 			// le serpent mange la nourriture
 			for (var i = 0; i < tabFood.length; i++) {
 				if (cell.x === tabFood[i].x && cell.y === tabFood[i].y) {
@@ -377,6 +389,133 @@ const play = (nbFruits, timingstamp, color, boolMur, ia, pixels, randInt) => {
 						document.getElementById("scoreNum").innerHTML = 0;
 					}
 				}
+			}
+			for (
+				var compteur = indexCopy;
+				compteur < snake.cells.length;
+				compteur++
+			) {
+				// Loop over every snake segment
+				console.log(
+					"index : ",
+					index,
+					"x: ",
+					snake.cells[compteur].x / grid,
+					"y: ",
+					snake.cells[compteur].y / grid,
+					"foodX :",
+					tabFood[0].x / grid,
+					"foodY :",
+					tabFood[0].y / grid
+				);
+				var cell = snake.cells[compteur];
+				var segx = cell.x;
+				var segy = cell.y;
+				var tilex = segx * grid;
+				var tiley = segy * grid;
+				// Sprite column and row that gets calculated
+				var tx = 0;
+				var ty = 0;
+				if (compteur == 0) {
+					// Head; Determine the correct image
+					if (snake.direction == "down") {
+						// Up
+						tx = 3;
+						ty = 0;
+					} else if (snake.direction == "right") {
+						// Right
+						tx = 4;
+						ty = 0;
+					} else if (snake.direction == "up") {
+						// Down
+						tx = 4;
+						ty = 1;
+					} else if (snake.direction == "left") {
+						// Left
+						tx = 3;
+						ty = 1;
+					}
+				} else if (compteur == snake.cells.length - 1) {
+					// Tail; Determine the correct image
+					var presseg = snake.cells[compteur - 1]; // Prev segment
+					if (presseg.y < segy) {
+						// Up
+						tx = 3;
+						ty = 2;
+					} else if (presseg.x > segx) {
+						// Right
+						tx = 4;
+						ty = 2;
+					} else if (presseg.y > segy) {
+						// Down
+						tx = 4;
+						ty = 3;
+					} else if (presseg.x < segx) {
+						// Left
+						tx = 3;
+						ty = 3;
+					}
+				} else {
+					// Body; Determine the correct image
+					var presseg = snake.cells[compteur - 1]; // Previous segment
+					var nextseg = snake.cells[compteur + 1]; // Next segmentconte
+					if (
+						(presseg.x < segx && nextseg.x > segx) ||
+						(nextseg.x < segx && presseg.x > segx)
+					) {
+						// Horizontal Left-Right
+						tx = 1;
+						ty = 0;
+					} else if (
+						(presseg.x < segx && nextseg.y > segy) ||
+						(nextseg.x < segx && presseg.y > segy)
+					) {
+						// Angle Left-Down
+						tx = 2;
+						ty = 0;
+					} else if (
+						(presseg.y < segy && nextseg.y > segy) ||
+						(nextseg.y < segy && presseg.y > segy)
+					) {
+						// Vertical Up-Down
+						tx = 2;
+						ty = 1;
+					} else if (
+						(presseg.y < segy && nextseg.x < segx) ||
+						(nextseg.y < segy && presseg.x < segx)
+					) {
+						// Angle Top-Left
+						tx = 2;
+						ty = 2;
+					} else if (
+						(presseg.x > segx && nextseg.y < segy) ||
+						(nextseg.x > segx && presseg.y < segy)
+					) {
+						// Angle Right-Up
+						tx = 0;
+						ty = 1;
+					} else if (
+						(presseg.y > segy && nextseg.x > segx) ||
+						(nextseg.y > segy && presseg.x > segx)
+					) {
+						// Angle Down-Right
+						tx = 0;
+						ty = 0;
+					}
+				}
+
+				// Draw the image of the snake part
+				context.drawImage(
+					image,
+					tx * 64,
+					ty * 64,
+					64,
+					64,
+					snake.cells[compteur].x,
+					snake.cells[compteur].y,
+					16,
+					16
+				);
 			}
 		});
 		function snakeIa() {
