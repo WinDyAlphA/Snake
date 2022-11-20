9d3887238d2aa1cd4dcc75fc19a7db5f62610adc
p2109548 <noah.heraud@etu.univ-lyon1.fr>
2022-11-20 23:47:06 +0100

    mobile playable

--- menu.js
+++ menu.js
@@ -776,6 +776,58 @@ const play = (
 	}
 
 	function addEvent() {
+		document.addEventListener("touchstart", handleTouchStart, false);
+		document.addEventListener("touchmove", handleTouchMove, false);
+
+		var xDown = null;
+		var yDown = null;
+
+		function getTouches(evt) {
+			return (
+				evt.touches || // browser API
+				evt.originalEvent.touches
+			); // jQuery
+		}
+
+		function handleTouchStart(evt) {
+			const firstTouch = getTouches(evt)[0];
+			xDown = firstTouch.clientX;
+			yDown = firstTouch.clientY;
+		}
+
+		function handleTouchMove(evt) {
+			if (!xDown || !yDown) {
+				return;
+			}
+
+			var xUp = evt.touches[0].clientX;
+			var yUp = evt.touches[0].clientY;
+
+			var xDiff = xDown - xUp;
+			var yDiff = yDown - yUp;
+
+			if (Math.abs(xDiff) > Math.abs(yDiff)) {
+				/*most significant*/
+				if (xDiff > 0) {
+					snake.dx = -grid;
+					snake.dy = 0;
+				} else {
+					snake.dx = grid;
+					snake.dy = 0;
+				}
+			} else {
+				if (yDiff > 0) {
+					snake.dy = -grid;
+					snake.dx = 0;
+				} else {
+					snake.dy = grid;
+					snake.dx = 0;
+				}
+			}
+			/* reset values */
+			xDown = null;
+			yDown = null;
+		}
 		document.addEventListener("keydown", function (e) {
 			// left arrow key
 			if (
